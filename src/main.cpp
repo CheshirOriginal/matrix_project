7ab8d53c69bdae0947824b583965d8a79909e8e9
Kirya <hghghghgh@mail.ru>
2026-02-22 20:56:36 +0300

    rollback

--- src/main.cpp
+++ src/main.cpp
@@ -73,7 +73,7 @@ int main() {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             if (i < j) {
-                matrix[i][j] = minValue + 1;
+                matrix[i][j] = minValue;
             }
         }
     }
